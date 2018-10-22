327dc01d1082da1ff721099aaefcbd1a9f2f71d0
Christopher Scharer <cdschar@emory.edu>
2018-10-22 08:52:48 -0400

    Update

--- refFlat2bed.R
+++ refFlat2bed.R
@@ -11,16 +11,15 @@ if(grepl("gz", file)){
 	dat = read.csv(file,header=F,sep="\t")
 }
 
+color = "79,129,189"
+
 col1 = c()
-col2 = c()
 for(i in 1:dim(dat)[1]){
 	x1 = unlist(strsplit(as.vector(dat[i,11]),","))
 	x2 = unlist(strsplit(as.vector(dat[i,10]),","))
 	len1 = paste(as.numeric(x1)-as.numeric(x2), collapse=",")
-	len2 = paste(as.numeric(x1)-dat[i,5], collapse=",")
 	col1 = c(col1, len1)
-	col2 = c(col2, len2)
 }
 
-bed_out = data.frame( dat[, c(3,5,6,1)],0,dat[,c(4,7,8)],0,dat[,9], col1, col2)
+bed_out = data.frame( dat[, c(3,5,6,1)],0,dat[,c(4,7,8)],color,dat[,9],col1,dat[,10])
 write.table(bed_out, out_file, quote=F, sep="\t", col.names=F, row.names=F)
